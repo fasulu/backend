6d938f541501e21dbdb99b631b66af50937b986d
fasulu <rfasulu@hotmail.com>
2021-05-27 17:33:35 +0200

    simple student form

--- chapitre_02/students/students/src/App.js
+++ chapitre_02/students/students/src/App.js
@@ -1,5 +1,6 @@
+
 // import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import StudentList from './views/StudentList';
@@ -7,26 +8,45 @@ import AddStudent from './views/AddStudent';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
 
-      <div >
+       <div >
 
         <h1>Students</h1>
 
         <Switch>
-          <Route path="/student" exact>
-            <StudentList />
+
+          <Route path="/" exact component={StudentList}/>
+
+
+
+          <Route path="/addstudent" exact>
+            <AddStudent />
           </Route>
 
-          {/* <Route path="/addStudent" exact>
-            <AddStudent />
-          </Route> */}
         </Switch>
 
       </div>
 
-    </Router>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import StudentList from './views/StudentList';
+// import AddStudent from './views/AddStudent';
+
+// function App() {
+//   return (
+//     <div >
+
+//         <h1>Students</h1>
+//       </div>
+//   );
+// }
+// export default App;
