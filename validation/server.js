d665d905de3b6b973b31739ce2ad4c59606608aa
fasulu <rfasulu@hotmail.com>
2021-06-18 15:34:57 +0200

    Validation another method to pick username, id & email in one request

--- validation/server.js
+++ validation/server.js
@@ -27,7 +27,7 @@ mongoose.connect("mongodb://localhost:27017/validation", { useNewUrlParser: true
 
 app.use("/users", router)
 
-//#region get userlist  here
+//#region get userlist here
 
 app.get("/users", debug, async (req, res) => {
 
@@ -49,7 +49,6 @@ app.get("/users", debug, async (req, res) => {
 })
 
 //#endregion
-
 
 //#region add new user with express-validator in to the database
 
@@ -101,13 +100,121 @@ app.post("/users/add", debug,
 
 //#endregion
 
+//#region get information by username, email or id
+
+// app.get("/users/:userinput", debug, async (req, res) => {
+
+//     console.log("IM IN GET USER INFO BY userinput", req.params.userinput)
+
+//     const userRequest = req.params.userinput;
+
+//     const emailExpression = /^\S+@\S+\.\S+$/; // for email this will also work === /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})+)$/;
+
+//     const idExpression = /^([a-z0-9]+)$/;      // ----> only small letters and numbers for mongodb _id field validation ,  this is to check is the number mixed in the string ---->   /\d/ is number mixed in a string
+
+//     const nameExpression = /^[a-z]+$/;    // only small letter alphabets
+
+//     console.log(emailExpression.test(userRequest))
+//     console.log(idExpression.test(userRequest))
+//     console.log(nameExpression.test(userRequest))
+
+//     try {
+
+//         if (emailExpression.test(userRequest)) {    //if email true
+
+//             console.log("Valid eMail", userRequest);
+
+//             const emailFound = await modelUser.findOne({ email: userRequest })
+
+//             res.json({
+//                 message: "User request is Valid eMail",
+//                 userRequest,
+//                 emailFound
+//             })
+
+//         } else if (/\d/.test(userRequest)) {    // if string with number(for id field)
+
+//             console.log("Valid ID", userRequest);
+
+//             searchFlag = "id";
+
+//             const idFound = await modelUser.findOne({ _id: userRequest })
+
+//             res.json({
+//                 message: "User request is Valid ID",
+//                 userRequest,
+//                 idFound
+//             })
+
+//         } else {
+
+//             console.log("Valid name", userRequest);     // else it is only alphabets which is name field
+
+//             const nameFound = await modelUser.findOne({ name: userRequest })
+
+//             res.json({
+//                 message: "User request is Valid name",
+//                 userRequest,
+//                 nameFound
+//             })
+//         }
+//     } catch (error) {
+//         console.log("User request is not identified....", userRequest)
+//         res.json({
+//             message: "User request is not identified",
+//             userRequest
+//         })
+//     }
+
+// })
+
+//#endregion
+
+//#region server listen
+
+app.listen(port, () => {
+    console.log("The server is listing in the port: ", port)
+})
+
+//#endregion
 
 
-//#region get information based on username
+//********************* different trail methods all working ********************/
+
+//#region tried with leandro method :email(\\S+@\\S+) works fine
+
+app.get("/users/:email(\\S+@\\S+)", debug, async (req, res) => {        // give regex validation in the route itself ***IMPORTANT INCLUDE EXTRA BACKSLASH WITH EVER BACKSLASHES IF NOT WILL GIVE OS ERROR***
+
+    console.log("IM IN GET USER INFO BY NAME", req.params.email);
+
+    const userToFind = req.params.email;
+
+    const userFound = await modelUser.findOne({ email: userToFind.trim() })
+
+    // console.log("username found in the collection", userFound);
+
+    if (userFound) {
+
+        console.log("User found ", userFound)
+        res.json({
+            message: "User Found",
+            userFound
+        })
+    } else {
+        res.json({
+            message: `Couldn't found requested user ${userToFind} in the list`
+        })
+    }
+
+})
+
+//#endregion
+
+//#region tried working
 
 // app.get("/users/:name", debug, async (req, res) => {
 
-//     console.log("IM IN GET USER INFO BY NAME", req.body.name)
+    //     console.log("IM IN GET USER INFO BY NAME", req.body.name)
 
 //     const userToFind = req.params.name;
 
@@ -132,123 +239,7 @@ app.post("/users/add", debug,
 
 //#endregion
 
-
-
-//#region get information by email
-
-app.get("/users/:userinput", debug, async (req, res) => {
-
-    console.log("IM IN GET USER INFO BY userinput", req.params.userinput)
-
-    let searchFlag = "";
-    console.log(searchFlag, "search Flag")
-
-    const userRequest = req.params.userinput;
-
-    const emailExpression = /^\S+@\S+\.\S+$/; // this will also work === /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,})+)$/;
-
-    const idExpression = /^([a-z0-9]+)$/;      //     /\d/ is number mixed in a string
-
-    const nameExpression = /^[a-z]+$/;
-
-    console.log(emailExpression.test(userRequest))
-    console.log(idExpression.test(userRequest))
-    console.log(nameExpression.test(userRequest))
-
-    try {
-
-        if (emailExpression.test(userRequest)) {
-
-            console.log("Valid eMail", userRequest);
-
-            searchFlag = "email";
-
-            res.json({
-                message: "User request is Valid eMail",
-                userRequest, searchFlag
-            })
-
-        } else if (/\d/.test(userRequest)) {
-
-            console.log("Valid ID", userRequest);
-
-            searchFlag = "id";
-
-            res.json({
-                message: "User request is Valid ID",
-                userRequest, searchFlag
-            })
-
-        } else {
-
-            console.log("Valid name", userRequest);
-
-            searchFlag = "name";
-
-            res.json({
-                message: "User request is Valid name",
-                userRequest, searchFlag
-            })
-        }
-    } catch (error) {
-        console.log("User request is not identified....", userRequest)
-        res.json({
-            message: "User request is not identified",
-            userRequest, searchFlag
-        })
-    }
-
-
-    // if (searchFlag === "email") {
-    //     const emailFound = await modelUser.findOne({ email: emailToFind })
-    //     console.log("User found ", emailFound)
-
-    //     res.json({
-    //         message: "User details as per email ID",
-    //         emailFound
-    //     })
-
-    // } else if (searchFlag === "id") {
-    //     const idFound = await modelUser.findOne({ _id: emailToFind })
-    //     console.log("User found ", idFound)
-
-    //     res.json({
-    //         message: "Valid ID format", emailToFind
-    //     })
-    // } else if (searchFlag === "name") {
-    //     const nameFound = await modelUser.findOne({ name: emailToFind })
-    //     console.log("User found ", emailFound)
-
-    //     res.json({
-    //         message: "Valid name format", nameToFind
-    //     })
-
-    // } else {
-    //     console.log("User request is not identified")
-    // }
-
-    // const emailFound = await modelUser.findOne({ email: emailToFind })
-
-    // console.log("user email found in the collection", emailFound);
-
-    // if (emailFound) {
-
-    //     console.log("User found ", emailFound)
-    //     res.json({
-    //         message: "User details as per email ID",
-    //         emailFound
-    //     })
-    // } else {
-    //     res.json({
-    //         message: `Couldn't found requested email ID ${emailToFind} in the list`
-    //     })
-    // }
-
-})
-
-//#endregion
-
-//#region get user details by id
+//#region tried working... get user details by id
 
 // app.get("/users/:id", debug, async (req, res) => {
 
@@ -278,7 +269,7 @@ app.get("/users/:userinput", debug, async (req, res) => {
 
 //#endregion
 
-//#region tried another way to find username and email
+//#region tried working ... another way to find username and email
 
 // app.get("/users/:userinput", debug, async (req, res) => {
 
@@ -325,7 +316,7 @@ app.get("/users/:userinput", debug, async (req, res) => {
 
 //#endregion
 
-//#region get information based on user id
+//#region tried working ... get information based on user id
 
 // app.get("/users/:id", debug, async (req, res) => {
 
@@ -364,10 +355,6 @@ app.get("/users/:userinput", debug, async (req, res) => {
 
 //#endregion
 
-//#region server listen
+//#region tried working ...  get information based on username
 
-app.listen(port, () => {
-    console.log("The server is listing in the port: ", port)
-})
-
-//#endregion
+//********************* ********************/
