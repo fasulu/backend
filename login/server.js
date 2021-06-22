bbc852a3f03773379576944278a0e4ac280bcc4c
fasulu <rfasulu@hotmail.com>
2021-06-22 15:21:56 +0200

    login bcrypt signup finished

--- login/server.js
+++ login/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require("cors")
 const mongoose = require('mongoose');
-const { body, validationResult } = require('express-validator');
 const app = express();
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 app.use(cors());
 app.use(express.json());
@@ -54,30 +56,47 @@ const userModel = mongoose.model("User", userSchema);
 //#endregion
 
 
-//#region add new user with express-validator in to the database
+//#region add new user with with post/signup
 
-app.post("/add",
+app.post("/signup",
     body("name").not().isEmpty().trim().escape().isLength({ min: 4, max: 20 }),
     body("passwd").not().isEmpty().trim().isLength({ min: 7, max: 25 }),
 
     async (req, res) => {
 
-        console.log("Im in user post", req.body)
+        console.log("Im in user signup", req.body)
+
         const userName = req.body.name
         const userpwd = req.body.passwd
-
+        
         const errorVal = validationResult(req)
-
-        if(errorVal.isEmpty()){
-
-                    const userAdded = await userModel.create({ name: userName, passwd: userpwd })
+        
+        try {
             
-                    res.json({
-                        message: "Iam in user post",
-                        userAdded
-                    })
+            if (errorVal.isEmpty()) {
+                
+                const password = bcrypt.hashSync(userpwd)
 
-        } else {
+                const userAdded = await userModel.create({ name: userName, passwd: password })
+
+                res.json({
+                    message: "Iam in user post",
+                    userAdded
+                })
+
+            } else {
+
+                console.log("Please verify your user name and password matches the regulation");
+
+                res.json({
+                    message: `Error while processing your ${userName} as new user`
+                })
+            }
+
+        } catch (error) {
+
+            console.log(`Error while adding new user :- ${userName}`, error);
+
             res.json({
                 message: `Error while processing your ${userName} as new user`,
             })
@@ -85,6 +104,36 @@ app.post("/add",
     })
 //#endregion
 
+//#region login user with post/login
+
+app.post("/login", async (req, res) => {
+
+    console.log("Im in post login")
+
+    const userName = req.body.name;
+    const userPwd = req.body.passwd;
+
+    console.log(userName, userPwd)
+    try {
+        
+        const givenPwd = bcrypt.hashSync(userPwd);
+    
+        const userDetail = await findOne({ name: userName })
+    
+        const isValidUser = bcrypt.compareSync(givenPwd, userDetail.passwd)
+    
+        console.log(isValidUser)
+
+    } catch (error) {
+        
+    }
+    res.json({
+
+        message:"Im in post login"
+    })
+})
+
+//#endregion
 
 //#region server listen
 
